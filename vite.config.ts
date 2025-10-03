8caafd06deb4404c8cd21010869456065c58f5a3
Vitalik-Kh <vtlkand@gmail.com>
2025-10-03 20:59:52 +0100

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/qa-engineer-cv-page/',
+  base: '/qa-engineer-page/',
   build: {
     outDir: 'dist',
     sourcemap: true
